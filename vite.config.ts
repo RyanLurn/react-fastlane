9488d09dceaf4c2ec163cf9871237fe24c5868d1
Ryan Luong <lumintriet@gmail.com>
2025-09-23 06:27:18 +0700

    Add path alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react";
+// biome-ignore lint/style/useNodejsImportProtocol: We use Bun here instead of Node
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
