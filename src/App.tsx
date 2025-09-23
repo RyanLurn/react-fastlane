166fc2e25baa69f73a18f697bc5bcbf62fde0fb9
Ryan Luong <lumintriet@gmail.com>
2025-09-23 09:31:42 +0700

    Add components from shadcn/ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <div>
-      <h1>React Fastlane</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 }
