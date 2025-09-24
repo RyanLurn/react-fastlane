32809c18588642cd01cbd9694b4e84fb41e66776
Ryan Luong <lumintriet@gmail.com>
2025-09-24 12:30:48 +0700

    Add util components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { ModeToggle } from "@/components/utils/mode-toggle";
+
 function App() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <ModeToggle className="fixed top-3 right-3" />
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         React Fastlane
       </h1>
